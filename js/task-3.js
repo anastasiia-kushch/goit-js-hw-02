399b0b8938959e52e1b9f6e193ac49c34cbd6dc4
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2023-10-23 15:04:30 +0200

    firsttry

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+function checkForSpam(message) {
+    const blackListWord1 = `spam`
+    const blackListWord2 = `sale`
+    console.log (message.toLowerCase().includes(blackListWord1) || message.toLowerCase().includes(blackListWord2));
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
